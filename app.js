392f9f7e16e17e8a5718687fa622955d28e01019
MrMafu <kinanradiaputra@gmail.com>
2025-02-08 12:03:39 +0700

    chore: add API controllers for routes users, tasks, userTask

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Import DB Connection
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var tasksRouter = require('./routes/tasks');
+var userTaskRouter = require('./routes/userTask');
 
 // Create Express App
 var app = express();
@@ -24,27 +28,36 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Define Route
+// Greeting API
 app.use('/', indexRouter);
+
+// Users API
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// Tasks API
+app.use('/tasks', tasksRouter);
+
+// User Task API
+app.use('/user-task', userTaskRouter);
+
+// Handle Error
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function (err, req, res) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error
+// set locals, only providing error in development
+// res.locals.message = err.message;
+// res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// render the error page
+// res.status(err.status || 500);
+// res.render('error');
 
 // Set port
-const port = process.env.APP_PORT || 4000;
+const port = process.env.APP_PORT || 8080;
+
+console.log(process.env.APP_PORT);
 
 // Start server
 app.listen(port, () => {
